4c0bddc69ca5358783f8b7c29aac35b8b0c8b3d7
Nathan Long <nrlong13@gmail.com>
2020-04-04 21:01:25 -0400

    updated functions to clear error when rendering html

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
 
 const employees = [];
@@ -109,7 +107,16 @@ function employeeAdd(){
 };
 
 employeeAdd();
-​
+
+function writeToHtml(){
+    fs.writeFileSync(outputPath, render(employees), "utf-8"),
+    function (err){
+        if (err){
+            throw err
+        }
+    };
+    console.log("Operation complete.  Your team is ready to be viewed!");
+};​
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 ​
